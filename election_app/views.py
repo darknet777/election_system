782275cc4dc88912afc066e515e3606eef4187c0
darknet777 <skyfallbladez@gmail.com>
2022-03-27 06:12:02 +0800

    Added statistic views

--- election_app/views.py
+++ election_app/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'election_app/index.html')
 
-def about(request):
-    return render(request, 'election_app/about.html')
+def statistic(request):
+    return render(request, 'election_app/statistic.html')
 
 def contact(request):
     return render(request, 'election_app/contact.html')
