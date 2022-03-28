3886424fcbd4590c1dc4b2a44a6ed58cff3ba8ca
darknet777 <skyfallbladez@gmail.com>
2022-03-29 02:27:07 +0800

    Created models

--- election_app/models.py
+++ election_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class EVS_Admin(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class Candidate(models.Model):
+    # Candidate info
+    candidate = models.CharField(max_length=255)
+    votes = models.IntegerField()
+
+class Voter(models.Model):
+    no_dpt = models.IntegerField()
