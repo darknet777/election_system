34987b2ed6cb342153bc2a7e783aa24d8b1cad30
darknet777 <skyfallbladez@gmail.com>
2022-03-29 02:26:38 +0800

    Integrated statistic view with database

--- election_app/views.py
+++ election_app/views.py
@@ -1,10 +1,19 @@
+from re import template
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import EVS_Admin, Candidate, Voter
 
 def index(request):
     return render(request, 'election_app/index.html')
 
 def statistic(request):
-    return render(request, 'election_app/statistic.html')
+    candidate = Candidate.objects.all().values()
+    template = loader.get_template('election_app/statistic.html')
+    context = {
+        'candidate': candidate,
+    }
+    return HttpResponse(template.render(context, request))
 
 def contact(request):
     return render(request, 'election_app/contact.html')
