d065b836be6a35f5ec589f7520fbf3e4b1ae351c
darknet777 <skyfallbladez@gmail.com>
2022-04-04 15:39:38 +0800

    Added validation views

--- election_app/views.py
+++ election_app/views.py
@@ -1,11 +1,19 @@
 from re import template
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from django.urls import reverse
 from .models import EVS_Admin, Candidate, Voter
 
 def index(request):
     return render(request, 'election_app/index.html')
+
+def validation(request):
+    x = request.POST['no_dpt']
+    if x == type(int):
+        return HttpResponse('Butul')
+    else:
+        return HttpResponse('Salah')
 
 def statistic(request):
     candidate = Candidate.objects.all().values()
