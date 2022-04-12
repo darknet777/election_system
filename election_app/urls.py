ff0b1214252ab6a6c5ae75f48c1860c0a75ad6cf
darknet777 <skyfallbladez@gmail.com>
2022-04-12 18:58:34 +0800

    Changed path name

--- election_app/urls.py
+++ election_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='election-home'),
-    path('dpt_validation/', views.validation, name='election-validation'),
+    path('validation/', views.validation, name='election-validation'),
     path('statistic/', views.statistic, name='election-statistic')
 ]
