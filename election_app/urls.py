57c7597a531c02aeb15914052ffb5cddc48b11d0
darknet777 <skyfallbladez@gmail.com>
2022-04-13 19:42:05 +0800

    Added voting path

--- election_app/urls.py
+++ election_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='election-home'),
     path('validation/', views.validation, name='election-validation'),
+    path('voting/', views.voting, name='election-voting'),
     path('statistic/', views.statistic, name='election-statistic')
 ]
